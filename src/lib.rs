872125783f626b87b3df2ce1f5f64dc2d7e1da7b
U-Jonesey13-PC\Jonesey13 <adj27@cam.ac.uk>
2015-08-06 19:58:29 +0100

    Inital Commit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+extern crate libc;
+extern crate winapi;
+extern crate kernel32;
+extern crate user32;
+
+use winapi::*;
+use kernel32::*;
+use user32::*;
+use std::ptr;
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+use std::mem;
+
+
+
+#[test]
+fn it_works() {
+}
